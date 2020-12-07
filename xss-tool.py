22ef09ba8b5f520811a37eb9ea0d3ca5ca261198
0xW00dy <gabriel.bureau@protonmail.com>
2020-12-07 21:44:59 +0100

    example window pygui

--- xss-tool.py
+++ xss-tool.py
@@ -1,6 +1,14 @@
+#!/usr/bin/python3
+from dearpygui import core, simple
+
 
 def main():
-    pass
+    with simple.window("h4x0r"):
+        core.add_text("H3110 W0r1d!")
+        core.add_input_text("string")
+        core.add_slider_float("float")
+    core.start_dearpygui()    
+
 
 
 if __name__ == '__main__':
